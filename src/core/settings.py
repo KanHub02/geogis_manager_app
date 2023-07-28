559b13a257d452fd6a7f27657361660bbaa44f2e
KanHub02 <usmnvk200207@gmail.com>
2023-07-28 23:06:34 +0600

    Static endpoints added, django.gis added in installed_apps

--- src/core/settings.py
+++ src/core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.gis',
     *THIRD_PARTY_APPS,
     *LOCAL_APPS,
 ]
--- src/location/admin.py
+++ src/location/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Canton, Contour, Region, District
+
+admin.site.register(Canton)
+admin.site.register(Contour)
+admin.site.register(Region)
+admin.site.register(District)
+
 # Register your models here.
