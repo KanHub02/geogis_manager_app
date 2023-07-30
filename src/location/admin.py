c941050c248fdf4b04443f3f47143807988808bc
KanHub02 <usmnvk200207@gmail.com>
2023-07-31 02:26:30 +0600

    Admin site refactoring, tabulars added

--- src/location/admin.py
+++ src/location/admin.py
@@ -2,9 +2,51 @@ from django.contrib import admin
 
 from .models import Canton, Contour, Region, District
 
-admin.site.register(Canton)
 admin.site.register(Contour)
-admin.site.register(Region)
-admin.site.register(District)
 
-# Register your models here.
+class ContourInline(admin.TabularInline):
+    model = Contour
+    fields = ["geometry", ]
+
+class RegionInline(admin.TabularInline):
+    model = Region
+    fields = ["title", "geometry", "is_deleted"]
+    readonly_fields = ["is_deleted"]
+
+class DistrictInline(admin.TabularInline):
+    model = District
+    fields = ["title", "geometry", "is_deleted"]
+    readonly_fields = ["is_deleted"]
+    
+class CantonInline(admin.TabularInline):
+    model = Canton
+    fields = ["title", "geometry", "is_deleted"]
+    readonly_fields = ["is_deleted"]
+    
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ["title", "id"]
+    list_display_links = ["title", "id"]
+    readonly_fields = ["id", "created_at", "updated_at"]
+    fields = ["title", "geometry", "is_deleted"]
+    inlines = [DistrictInline, ]
+
+
+@admin.register(Canton)
+class CantonAdmin(admin.ModelAdmin):
+    list_display = ["title", "id"]
+    list_display_links = ["title", "id"]
+    readonly_fields = ["id", "created_at", "updated_at"]
+    fields = ["title", "geometry", "is_deleted"]
+    inlines = [ContourInline,]
+
+
+@admin.register(District)
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ["title", "id"]
+    list_display_links = ["title", "id"]
+    readonly_fields = ["id", "created_at", "updated_at"]
+    fields = ["title", "geometry", "is_deleted"]
+    inlines = [CantonInline]
+
