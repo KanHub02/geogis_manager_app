fe90cfc3e3b8d9df5f340cb74f7292db8d5c56bb
KanHub02 <usmnvk200207@gmail.com>
2023-07-30 21:09:01 +0600

    file rename

--- src/common/management/commands/generate_geo_data.py
+++ src/common/management/commands/generate_geo_data.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from django.core.management.base import BaseCommand
 
-from scripts.create_geo_data import GeographyDataService
+from scripts.services import GeographyDataService
 
 
 class Command(BaseCommand):
